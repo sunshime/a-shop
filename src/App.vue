7debcee06324d4e81e079193663ceda516abecd7
lfj <liao_fengji@163.com>
2019-06-26 14:48:05 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,21 @@
 
 <script>
 export default {
-  name: 'App'
+  // name: "App"
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
+  overflow-x: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
 }
 </style>
